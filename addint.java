22b3165425deb8d7d68bbce8c2ed24dde277a4a1
Khushi Sharma <70655479+itskhushis1@users.noreply.github.com>
2022-05-18 23:08:35 +0530

    Create addint.java

---
+++ addint.java
@@ -0,0 +1,25 @@
+package may14;
+
+public class addarr {
+
+	public static void main (String args[]){
+        int n = 5000000;
+        int[] arr = new int [n];
+        for (int i = 0; i < n ; i++)
+              arr[i] = i;
+        
+
+      // Calculate sum in O(n) time
+        long start = System.nanoTime();
+
+        int sum = 0;
+        for (int i = 0; i<n; i++)
+        	 sum= sum + arr[i];
+        System.out.println(sum);
+        long end = System.nanoTime();
+
+        long elapsedTime = end - start;
+        System.out.println(elapsedTime);
+	}
+
+}
